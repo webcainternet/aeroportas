4e797ac3ec4e1b85ac4a4f06e6563f23147e2d45
root <root@uspro575.startdedicated.com>
2016-02-18 00:40:41 -0200

    upfiles

--- public/admin/config.php
+++ public/admin/config.php
@@ -8,23 +8,23 @@ define('HTTPS_SERVER', 'http://aeroportas.homolog.lojavirtual.digital/admin/');
 define('HTTPS_CATALOG', 'http://aeroportas.homolog.lojavirtual.digital/');
 
 // DIR
-define('DIR_APPLICATION', '/Users/fernandomendes/github/aeroportas/public/admin/');
-define('DIR_SYSTEM', '/Users/fernandomendes/github/aeroportas/public/system/');
-define('DIR_LANGUAGE', '/Users/fernandomendes/github/aeroportas/public/admin/language/');
-define('DIR_TEMPLATE', '/Users/fernandomendes/github/aeroportas/public/admin/view/template/');
-define('DIR_CONFIG', '/Users/fernandomendes/github/aeroportas/public/system/config/');
-define('DIR_IMAGE', '/Users/fernandomendes/github/aeroportas/public/image/');
-define('DIR_CACHE', '/Users/fernandomendes/github/aeroportas/public/system/cache/');
-define('DIR_DOWNLOAD', '/Users/fernandomendes/github/aeroportas/public/system/download/');
-define('DIR_UPLOAD', '/Users/fernandomendes/github/aeroportas/public/system/upload/');
-define('DIR_LOGS', '/Users/fernandomendes/github/aeroportas/public/system/logs/');
-define('DIR_MODIFICATION', '/Users/fernandomendes/github/aeroportas/public/system/modification/');
-define('DIR_CATALOG', '/Users/fernandomendes/github/aeroportas/public/catalog/');
+define('DIR_APPLICATION', '/srv/httpd/aeroportas.com.br/aeroportas/public/admin/');
+define('DIR_SYSTEM', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/');
+define('DIR_LANGUAGE', '/srv/httpd/aeroportas.com.br/aeroportas/public/admin/language/');
+define('DIR_TEMPLATE', '/srv/httpd/aeroportas.com.br/aeroportas/public/admin/view/template/');
+define('DIR_CONFIG', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/config/');
+define('DIR_IMAGE', '/srv/httpd/aeroportas.com.br/aeroportas/public/image/');
+define('DIR_CACHE', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/cache/');
+define('DIR_DOWNLOAD', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/download/');
+define('DIR_UPLOAD', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/upload/');
+define('DIR_LOGS', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/logs/');
+define('DIR_MODIFICATION', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/modification/');
+define('DIR_CATALOG', '/srv/httpd/aeroportas.com.br/aeroportas/public/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_USERNAME', 'aeroportas');
+define('DB_PASSWORD', 'h3y6dgf3h3');
 define('DB_DATABASE', 'aeroportas');
 define('DB_PREFIX', 'oc_');
--- public/config.php
+++ public/config.php
@@ -6,22 +6,22 @@ define('HTTP_SERVER', 'http://aeroportas.homolog.lojavirtual.digital/');
 define('HTTPS_SERVER', 'http://aeroportas.homolog.lojavirtual.digital/');
 
 // DIR
-define('DIR_APPLICATION', '/Users/fernandomendes/github/aeroportas/public/catalog/');
-define('DIR_SYSTEM', '/Users/fernandomendes/github/aeroportas/public/system/');
-define('DIR_LANGUAGE', '/Users/fernandomendes/github/aeroportas/public/catalog/language/');
-define('DIR_TEMPLATE', '/Users/fernandomendes/github/aeroportas/public/catalog/view/theme/');
-define('DIR_CONFIG', '/Users/fernandomendes/github/aeroportas/public/system/config/');
-define('DIR_IMAGE', '/Users/fernandomendes/github/aeroportas/public/image/');
-define('DIR_CACHE', '/Users/fernandomendes/github/aeroportas/public/system/cache/');
-define('DIR_DOWNLOAD', '/Users/fernandomendes/github/aeroportas/public/system/download/');
-define('DIR_UPLOAD', '/Users/fernandomendes/github/aeroportas/public/system/upload/');
-define('DIR_MODIFICATION', '/Users/fernandomendes/github/aeroportas/public/system/modification/');
-define('DIR_LOGS', '/Users/fernandomendes/github/aeroportas/public/system/logs/');
+define('DIR_APPLICATION', '/srv/httpd/aeroportas.com.br/aeroportas/public/catalog/');
+define('DIR_SYSTEM', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/');
+define('DIR_LANGUAGE', '/srv/httpd/aeroportas.com.br/aeroportas/public/catalog/language/');
+define('DIR_TEMPLATE', '/srv/httpd/aeroportas.com.br/aeroportas/public/catalog/view/theme/');
+define('DIR_CONFIG', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/config/');
+define('DIR_IMAGE', '/srv/httpd/aeroportas.com.br/aeroportas/public/image/');
+define('DIR_CACHE', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/cache/');
+define('DIR_DOWNLOAD', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/download/');
+define('DIR_UPLOAD', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/upload/');
+define('DIR_MODIFICATION', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/modification/');
+define('DIR_LOGS', '/srv/httpd/aeroportas.com.br/aeroportas/public/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_USERNAME', 'aeroportas');
+define('DB_PASSWORD', 'h3y6dgf3h3');
 define('DB_DATABASE', 'aeroportas');
 define('DB_PREFIX', 'oc_');
