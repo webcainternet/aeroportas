b7ccebb9401a052153f9a15f9b451a6c21f00a0e
fernandofmendes <fernando.mendes@webca.com.br>
2016-02-26 03:00:10 -0300

    Revert "panda plugin"
    This reverts commit 9f5800eb41b0f91f7a1563dd8cc01fb9f2f8853c.

--- public/upload/catalog/language/english/module/customproduct.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Heading 
-$_['heading_title'] = 'Featured v2 (Panda-Code)';
-
-// Text
-$_['text_reviews']  = 'Based on %s reviews.'; 
-?>
--- public/upload/catalog/language/russian/module/customproduct.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Heading 
-$_['heading_title'] = 'Рекомендуемые v2 (Panda-Code)';
-
-// Text
-$_['text_reviews']  = 'На основании %s продаж.'; 
-?>
